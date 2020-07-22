5aaa15fdae9e1f670a38481856464ead3be0ec25
Hampton (Bandwagon) <hampton@bandwagonfanclub.com>
2020-07-22 14:52:24 -0400

    A little cleanup.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 set -e
+
+GITHUB_PUBLIC_KEY="AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
 
 if [[ ! -d /vagrant ]]; then
     echo "There's no /vagrant directory, so I'm nervous you're running"
@@ -10,11 +14,9 @@ fi
 if [[ ! -f /etc/ssh/ssh_known_hosts ]] \
         || ! grep github.com /etc/ss/ssh_known_hosts &> /dev/null; then
     mkdir -p /etc/ssh
-    ssh-keyscan github.com > /etc/ssh/ssh_known_hosts 2> /dev/null
-    if [[ "$?" -ne "0" ]]; then
-        echo "Couldn't keyscan github?  Try again later?"
-        exit 1
-    fi
+    
+    # Baked-in results of `ssh-keyscan github.com`.
+    echo 'github.com ssh-rsa $GITHUB_PUBLIC_KEY' > /etc/ssh/ssh_known_hosts
 fi
 
 # Windows seems to have some issue making ssh-agent available during provision.
@@ -46,6 +48,7 @@ if [[ -d 'vagrant-modules' ]]; then
     echo 'Checking vagrant-modules/v1 for updates...'
     cd 'vagrant-modules'
     git reset --hard HEAD &> /dev/null
+    git clean -df &> /dev/null
     git pull &> /dev/null
 
     if [[ "$?" -ne "0" ]]; then
